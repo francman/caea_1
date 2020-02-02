a057f9b63be4fa7bd1663b42aa479baad7b43f17
francman <frankmanu500@gmail.com>
2020-02-02 11:40:33 -0500

    bins added

--- probability.c
+++ probability.c
@@ -1,14 +1,66 @@
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
+
+int binSorter(float randomNumber);
 
 #define N_EVENT 10000
+#define MBIN 10
 
 int main(int argc, char** argv[]){
 
-    int randomNumberArray[N_EVENT - 1];
+    float binWidth = 1/(double)MBIN; //BIN_WIDTH
+    float randomNumberArray[N_EVENT - 1] = {0};
+    int binArray[MBIN-1] = {0};
 
+//Generating random numbers
     for (int i=0; i<N_EVENT;i++){
-        randomNumberArray[i] = rand()/RAND_MAX;
+        randomNumberArray[i] = rand()/(double)RAND_MAX;
+        //printf("%d", randomNumberArray[i]);
+        binArray[binSorter(randomNumberArray[i])] += 1;
     }
 
+    for(int i =0; i<MBIN; i++){
+      printf("Bin # %d has %d \n", i, binArray[i]);
+    }
+
+return 0;
+}
+
+
+
+int binSorter(float randomNumber){
+  int bin = 0;
+
+  if ((randomNumber > 0) && (randomNumber <= 0.1)){
+      bin = 0;
+  }
+  else if ((randomNumber > 0.1) && (randomNumber <= 0.2)){
+      bin = 1;
+  }
+  else if ((randomNumber > 0.2) && (randomNumber <= 0.3)){
+      bin = 2;
+  }
+  else if ((randomNumber > 0.3) && (randomNumber <= 0.4)){
+      bin = 3;
+  }
+  else if ((randomNumber > 0.4) && (randomNumber <= 0.5)){
+      bin = 4;
+  }
+  else if ((randomNumber > 0.5) && (randomNumber <= 0.6)){
+      bin = 5;
+  }
+  else if ((randomNumber > 0.6) && (randomNumber <= 0.7)){
+      bin = 6;
+  }
+  else if ((randomNumber > 0.7) && (randomNumber <= 0.8)){
+      bin = 7;
+  }
+  else if ((randomNumber > 0.8) && (randomNumber <= 0.9)){
+      bin = 8;
+  }
+  else if ((randomNumber > 0.9) && (randomNumber <= 1.0)){
+      bin = 9;
+  }
+
+  return bin;
 }
